9f25cdd7d0f4e909d00c67872337139818f4f8fe
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-05-23 12:48:20 +0200

    added gifs

--- index.js
+++ index.js
@@ -83,6 +83,11 @@ function displayWeatherInfo(data) {
     card.appendChild(humidityDisplay);
     card.appendChild(descDisplay);
     card.appendChild(weatherEmoji);
+
+
+    card.className = 'card';
+    const weatherGif = getWeatherGif(id);
+    card.classList.toggle(weatherGif);
 }
 
 function getWeatherEmoji(weatherId) {
@@ -112,6 +117,28 @@ function getWeatherEmoji(weatherId) {
             return "❓";
     }
 }
+
+function getWeatherGif(weatherId) {
+    switch (true) {
+        case (weatherId >= 200 && weatherId < 300):
+            return "thunderstorm";
+        case (weatherId >= 300 && weatherId < 400):
+            return "drizzle";
+        case (weatherId >= 500 && weatherId < 600):
+            return "rain";
+        case (weatherId >= 600 && weatherId < 700):
+            return "snow";
+        case (weatherId >= 700 && weatherId < 800):
+            return "fog";
+        case (weatherId === 800):
+            return "clear_sky";
+        case (weatherId >= 801 && weatherId < 810):
+            return "clouds";
+        default:
+            return "boh";
+    }
+}
+
 
 function displayError(message) {
     const errorDisplay = document.createElement("p");
